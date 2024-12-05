50dc5ba53885a506b026967dd47d52f053c9e17f
Kaan-1 <kerdogan20@ku.edu.tr>
2024-12-05 14:01:42 +0300

    added benchmarker dependancy to scripts Cargo.toml

--- sp1_project/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/deps/fibonacci_program-bbbae629b15fbc29.d
+++ sp1_project/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/deps/fibonacci_program-bbbae629b15fbc29.d
@@ -1,8 +1,5 @@
-/home/kaan/Belgeler/VScode/Rust/zkvm_benchmarking_project/sp1_benchmarks/sp1_project/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/deps/fibonacci_program-bbbae629b15fbc29: program/src/main.rs program/src/vec_sum10.rs program/src/vec_sum100.rs program/src/vec_sum10000.rs
+/home/kaan/Belgeler/VScode/Rust/zkvm_benchmarking_project/sp1_benchmarks/sp1_project/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/deps/fibonacci_program-bbbae629b15fbc29: program/src/main.rs
 
-/home/kaan/Belgeler/VScode/Rust/zkvm_benchmarking_project/sp1_benchmarks/sp1_project/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/deps/fibonacci_program-bbbae629b15fbc29.d: program/src/main.rs program/src/vec_sum10.rs program/src/vec_sum100.rs program/src/vec_sum10000.rs
+/home/kaan/Belgeler/VScode/Rust/zkvm_benchmarking_project/sp1_benchmarks/sp1_project/target/elf-compilation/riscv32im-succinct-zkvm-elf/release/deps/fibonacci_program-bbbae629b15fbc29.d: program/src/main.rs
 
 program/src/main.rs:
-program/src/vec_sum10.rs:
-program/src/vec_sum100.rs:
-program/src/vec_sum10000.rs:
